7d84e24d232f7f78426fa565ebcee671e927149f
jsdami <noreply>
2015-11-26 02:50:28 +0000

    Add component frame

---
+++ ui/frame.reel/frame.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/frame.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Frame
+ * @extends Component
+ */
+exports.Frame = Component.specialize(/** @lends Frame# */ {
+    constructor: {
+        value: function Frame() {
+            this.super();
+        }
+    }
+});
